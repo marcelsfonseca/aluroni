a837b4e6603c3f10e1ed6614ead1d2916db4782c
Marcel S Fonseca <marcelsfonseca@gmail.com>
2022-04-23 17:04:22 -0300

    Criando o buscador

--- src/Pages/Cardapio/index.tsx
+++ src/Pages/Cardapio/index.tsx
@@ -1,18 +1,26 @@
 import styles from "./Cardapio.module.scss"
-import {ReactComponent as Logo} from 'assets/logo.svg';
-
+import { ReactComponent as Logo } from 'assets/logo.svg';
+import Buscador from "./Buscador";
+import { useState } from "react";
 
 export default function Cardapio() {
-    return(
+    const [busca, setBusca] = useState("");
+
+    return (
         <main>
             <nav className={styles.menu}>
-                <Logo/>
+                <Logo />
             </nav>
             <header className={styles.header}>
                 <div className={styles.header__text}>
                     A casa do Código e da Massa!
                 </div>
             </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardapio</h3>
+                <Buscador
+                    busca={busca} setBusca={setBusca} />
+            </section>
         </main>
     )
 }
